76f81a8585e8b110f027958586245f5626c01851
LeoDivine <divineonyi2004@gmail.com>
2024-10-11 12:26:20 +0100

    fix:minor issues

--- src/app/(root)/search/page.tsx
+++ src/app/(root)/search/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function SearchDriver() {
   return (
     <div className="max-w-[500px] p-2 mx-auto my-[100px]">
-      <AdvancedSearch placeholder="Enter vehicle plate" variant="primary" />
+      <AdvancedSearch placeholder="Search vehicle Plate, Name and Tcode" variant="primary" />
     </div>
   );
 }
